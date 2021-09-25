d4982727b8c3ed4f95034448b625e8d41fd6e3b4
Amdio Rocha <amdio0811@gmail.com>
2021-09-25 20:29:31 +0900

    Banner ao abrir a pagina

--- principal.js
+++ principal.js
@@ -4,5 +4,5 @@ var bannerAtual = 0;
 function trocaBanner() {   bannerAtual = (bannerAtual + 1) % 2; document.querySelector('h2#mensagem').textContent =      
 banners[bannerAtual]; 
 } 
-setInterval(trocaBanner, 2000); 
+setInterval(trocaBanner, 1000); 
 Precisamos também adicionar o script anterior à nossa página index.html, além de uma tag h2 que vai conter a mensagem: 
