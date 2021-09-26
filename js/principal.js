9c99d48f85d22837fe1f898056710dd66dc38575
Amdio Rocha <amdio0811@gmail.com>
2021-09-26 15:44:49 +0900

    Alterando textos do banner

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,5 @@
 var banners =      
-["Os melhores do Brasil!", "Qualidade e preço baixo!"]; 
+["Do lixo ao luxo!", "Reaproveitar é aproveitar!"]; 
 var bannerAtual = 0; 
 function trocaBanner() {   bannerAtual = (bannerAtual + 1) % 2; document.querySelector('h2#mensagem').textContent =      
 banners[bannerAtual]; 
